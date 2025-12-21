9891c1d9be72aefbf9b6c962261b9b485dd5e40f
Kos Tarasenko <pythokostas@gmail.com>
2025-12-21 18:27:14 +0300

    initial_commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+import config
+
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start_command(message):
+    print(message)
+
+
+@dp.message()
+async def all_messages(message: Message, bot: Bot):
+    msg_text = f'Пользователь {message.from_user.full_name} написал: \n{message.text}'
+    await bot.send_message(
+        # chat_id= 1308031077,
+        text=msg_text,
+    )
+
+async def start_bot():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(start_bot())
